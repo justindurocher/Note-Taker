d0d56e9a1d451b2ba11a0f81d349380fb19a4053
Justin Durocher <justduro@gmail.com>
2020-04-06 13:15:52 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var path = reequire("path");
+
+var fs = require("fs");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.urlencoded({ extended : true}));
+app.use(express.json());
+app,use(express.static("assets"));
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.get("/notes", function(req,res){
+    res.sendFile(path.join(__dirname, "notes.html"));
+})
